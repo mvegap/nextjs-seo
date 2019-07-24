b54e53f61c1c0a1c11bea656c92ba463466f7b9e
Miguel Vega <mvegaplm@gmail.com>
2019-07-24 12:27:11 -0500

    init project

---
+++ pages/about.jsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header';
+
+export default function Index() {
+  return (
+    <div>
+      <Header />
+      <p>This is a ABOUT PAGE on Next.js</p>
+    </div>
+  );
+}
